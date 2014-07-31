8371d85f8249e9a7b55d159fb8c8e38c7b3ccf83
Andrey Golovin <caldwell@inbox.ru>
2014-07-31 19:02:27 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,70 @@
+/**
+ * Created by andrey on 31.07.14.
+ */
+moment.locale('ru');
+var app = angular.module('App', ['ui'])
+	.controller('MyController', function($scope) {
+		// datepicker on select action
+		var onSelect = function(date, obj) {
+			var
+				$this = $(obj.input),
+				id = $this.attr('id'),
+				$dateTo = $('#date-to'),
+				$sportFrom = $('#sport-from'),
+				$sportTo = $('#sport-to'),
+				thisDate = moment(date, 'L').format('L'),
+				nextDate = moment(date, 'L').add(7, 'days').format('L');
+
+
+			if (id === 'date-from') {
+
+				$dateTo.datepicker('option', {
+					minDate: thisDate
+				});
+
+				if ($dateTo.attr('data-set-by-user') !== 'true') {
+					$dateTo.datepicker('setDate', nextDate);
+				}
+
+				$sportFrom.datepicker('option', {
+					minDate: thisDate
+				});
+				$sportFrom.datepicker('setDate', thisDate);
+				
+				$sportTo.datepicker('option', {
+					minDate: thisDate
+				});
+				$sportTo.datepicker('setDate', thisDate);
+				
+			} else if (id === 'date-to') {
+
+				$this.attr('data-set-by-user', 'true');
+
+			}
+
+		};
+
+		// datepicker options
+		$scope.dateOptions = {
+			changeYear: true,
+			changeMonth: true,
+			yearRange: '1900:+1',
+			minDate: '+1',
+			dateFormat: 'dd.mm.yy',
+			onSelect: onSelect
+		};
+
+		$scope.days = 0;
+
+		$scope.updateDays = function() {
+			var to = moment($('#date-to').datepicker('getDate'), 'L'),
+				from = moment($('#date-from').datepicker('getDate'), 'L');
+
+			$scope.days = to.diff(from, 'days');
+		};
+
+		$scope.dateFrom = new Date(moment().add(1, 'days').format());
+		$scope.dateTo = new Date(moment().add(7, 'days').format());
+		$scope.sportFrom = new Date(moment().add(1, 'days').format());
+		$scope.sportTo = new Date(moment().add(7, 'days').format());
+	});
